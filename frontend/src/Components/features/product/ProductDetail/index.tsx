30affed6517dd37f142d595dd30abf62f37cb3f5
Vaxied <psgmarcelo@gmail.com>
2024-11-07 20:09:21 -0400

    Added previous commit components to ProductDetail

--- frontend/src/Components/features/product/ProductDetail/index.tsx
+++ frontend/src/Components/features/product/ProductDetail/index.tsx
@@ -1,6 +1,11 @@
+import React from 'react'
 import NotFound from '@pages/NotFound'
 import useShoppingCart from '@hooks/useShoppingCart'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import ProductImgCarousel from '@components/features/carousel/ProductImgCarousel'
+import StarIcon from '@components/icons/StarIcon'
+import ProductImgSelector from '../ProductImageSelector'
+import DetailedProductDescription from '../DetailedProductDescription'
 
 // type props = {
 //     product: IProduct
@@ -8,7 +13,6 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 // }
 
 function ProductDetail() {
-    //check if product.id exists or send to notfound
     const { addItemToShoppingCart } = useShoppingCart()
     const navigate = useNavigate()
     const { state } = useLocation()
@@ -17,72 +21,137 @@ function ProductDetail() {
     console.log('params', params)
     const id = params['productId']
     console.log('product id is: ', id)
+
+    // TODO
+    // Save images in db/fs and fetch it once to stop making a call each time image is updated
+    const productImages = React.useRef([
+        {
+            src: product.image,
+            alt: 'something',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1583623733237-4d5764a9dc82?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'something',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1473188588951-666fce8e7c68?q=80&w=1874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'something',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1484527273420-c598cb0601f8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'something',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1531938716357-224c16b5ace3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'something',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1575032617751-6ddec2089882?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'something',
+        },
+    ])
+
+    const [imageToShow, setImageToShow] = React.useState(product.image)
+    //check if product.id exists or send to notfound
     if (!product) {
         return <NotFound />
     }
 
     return (
-        <div className='flex bg-gray-100 bg-opacity-50 w-full h-full px-8'>
-            <div className='flex justify-center w-[33%] h-full'>
-                <div>
-                    <figure className='relative mb-2 w-full h-[300px]'>
-                        <img
-                            className='w-full h-full object-contain rounded-lg'
-                            src={product.image}
-                            alt={product.title}
+        <>
+            <div className='flex gap-4 bg-gray-100 bg-opacity-50 w-full h-[calc(100vh-96px)] px-8'>
+                <div className='flex w-[33%] mr-4'>
+                    <div className='flex w-full top-[96px] sticky self-start gap-4'>
+                        <ProductImgSelector
+                            productImages={productImages.current}
+                            setImageToShow={setImageToShow}
                         />
-                    </figure>
+                        <div className='flex justify-center max-h-[30rem] flex-1'>
+                            <figure className='flex justify-center mb-2 w-full h-[300px]'>
+                                <img
+                                    className='min-w-52 w-full h-full object-contain'
+                                    src={imageToShow}
+                                    alt={product.title}
+                                />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-[calc(100%-33%-25%)]'>
+                    <p className='flex flex-wrap pb-2'>
+                        <span className='w-full text-md text-black/60 font-bold'>
+                            {product.title}
+                        </span>
+                        <span className='px-2 py-1 text-sm border border-gray rounded-lg bg-accent/70 text-black'>
+                            {product.category}
+                        </span>
+                    </p>
+                    <div className='flex items-center h-7 pb-3'>
+                        <span className='font-medium pr-1'>
+                            {product.average_rating.substring(0, 3)}
+                        </span>
+                        <span>
+                            <StarIcon />
+                        </span>
+                        <span className='pl-1 font-normal'>
+                            ({product.rating_count})
+                        </span>
+                    </div>
+                    <hr className='mb-3'></hr>
+                    <p className='flex items-start text-3xl leading-none'>
+                        <span className='text-sm'>US$</span>
+                        <span>{product.price.split('.')[0]}</span>
+                        <span className='text-sm'>
+                            {product.price.split('.')[1]}
+                        </span>
+                    </p>
+                    <p className='pb-4'>{product.description}</p>
+                    <ul className='pb-4 list-disc ml-4'>
+                        <li>Material: Premium leather</li>
+                        <li>
+                            Hardware: Gold-tone Interior: Spacious with multiple
+                            pockets
+                        </li>
+                        <li>
+                            Versatile Use: Ideal for day-to-night transitions
+                        </li>
+                        <li>Style: Adds a touch of luxury to any outfit</li>
+                    </ul>
+                    <p className='pb-4'>
+                        Step out in style with this sophisticated elegance
+                        handbag. Crafted from premium leather, its sleek design
+                        features a chic silhouette complemented by gold-tone
+                        hardware. The spacious interior, complete with multiple
+                        pockets, ensures all your essentials are organized and
+                        easily accessible. Perfect for day-to-night transitions,
+                        this handbag adds a touch of luxury to any outfit.
+                    </p>
+                </div>
+                <div className='w-[25%]'>
+                    <div className='flex flex-wrap bg-container p-4 rounded-lg'>
+                        <p className='pb-4'>
+                            Get it delivered to your doorstep by 25th Oct.
+                        </p>
+                        <div className='flex justify-end w-full'>
+                            <button
+                                type='button'
+                                onClick={(event) => {
+                                    addItemToShoppingCart(event, product)
+                                }}
+                                className='flex justify-center items-center text-md h-6 bg-secondary cursor-pointer border border-gray rounded-lg text-white p-4 self-center'
+                            >
+                                <span className='text-sm text-center font-semibold'>
+                                    Add to cart
+                                </span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='w-[calc(100%-33%)]'>
-                <p>
-                    <span className='text-md text-black/60 font-bold'>
-                        {product.title}
-                    </span>
-                </p>
-                <span className='px-2 w-[8rem] border border-gray rounded-lg bg-gray-300 text-black'>
-                    {product.category}
-                </span>
-                <hr className='mt-2'></hr>
-                <p className='flex items-start pt-2 text-3xl leading-none'>
-                    <span className='text-sm'>US$</span>
-                    <span>{product.price.split('.')[0]}</span>
-                    <span className='text-sm'>
-                        {product.price.split('.')[1]}
-                    </span>
-                </p>
-                <p className='flex items-center text-lg text-black font-semibold py-2'>
-                    <button
-                        type='button'
-                        onClick={(event) => {
-                            addItemToShoppingCart(event, product)
-                        }}
-                        className='flex justify-center items-center text-md h-6 bg-black cursor-pointer border border-gray rounded-lg text-white p-4'
-                    >
-                        <span className='text-sm font-semibold'>
-                            Add to cart
-                        </span>
-                        <span className='scale-x-[-1] ml-1'>
-                            <svg
-                                xmlns='http://www.w3.org/2000/svg'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                strokeWidth={1.5}
-                                stroke='currentColor'
-                                className='w-4 h-4'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
-                                />
-                            </svg>
-                        </span>
-                    </button>
-                </p>
-                <p className='pb-4'>{product.description}</p>
-            </div>
-        </div>
+            <hr className='mb-3' />
+            <ProductImgCarousel />
+            <DetailedProductDescription />
+        </>
     )
 }
 
