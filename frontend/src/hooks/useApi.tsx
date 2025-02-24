bcb3d26eedce03052a114927946523aade97a8cc
Vaxied <psgmarcelo@gmail.com>
2025-02-24 16:43:36 -0400

    (feat): now api calls can be retried 3 times when attempting to load a resource
    (refactor): extracted code from the useEffect in useApi

--- frontend/src/hooks/useApi.tsx
+++ frontend/src/hooks/useApi.tsx
@@ -22,19 +22,72 @@ function useApi() {
         terms,
         setTerms,
         logOut,
+        firstLoad,
     } = React.useContext(StoreContext) as StoreContextType
 
     const [tries, setTries] = React.useState(0)
+
+    const getOrders = async () => {
+        setIsLoading(true)
+        try {
+            const orders = await loadResource('json', '/get-orders')
+            setOrders(orders)
+        } catch (error: any) {
+            console.log('error getting orders', error)
+        }
+    }
+
+    const getCart = async () => {
+        try {
+            const products = await loadResource('json', '/load-cart')
+            products
+                ? setShoppingCartProducts(products)
+                : console.log(
+                      "User has no products in cart or products couldn't be fetched",
+                  )
+        } catch (error: any) {
+            console.log('error getting cart', error)
+        }
+    }
+
+    const getUser = async () => {
+        setIsLoading(true)
+        try {
+            const user = await loadResource('json', '/refresh-user')
+            if (user) {
+                if (user.error) {
+                    logOut()
+                }
+                console.log('response.status', user?.status)
+                if (user?.first_name) {
+                    console.log('RETURNED USER AFTER FIRST LOAD', user)
+                    setUsername(user.first_name)
+                    setLoggedIn(true)
+                }
+            }
+        } catch (error: any) {
+            console.log('error getting user', error)
+        }
+    }
+
+    const getItems = async () => {
+        setTries(0)
+        setIsLoading(true)
+        try {
+            const items = await loadResource('json')
+            if (items) {
+                setItems(items.info)
+            }
+        } catch (error: any) {
+            console.log('error getting items', error)
+        }
+    }
+
     React.useEffect(() => {
         try {
             if (loggedIn) {
                 if (!Array.isArray(orders) || !orders.length) {
-                    const loadOrders = async () => {
-                        setTries(0)
-                        setIsLoading(true)
-                        const orders = await loadResource('json', '/get-orders')
-                        setOrders(orders)
-                    }
+                    const loadOrders = async () => await getOrders()
                     loadOrders()
                 }
                 if (
@@ -42,50 +95,23 @@ function useApi() {
                     !shoppingCartProducts.length
                 ) {
                     console.log('TRYING TO LOAD PRODUCTS')
-                    const loadCart = async () => {
-                        setTries(0)
-                        const products = await loadResource(
-                            'json',
-                            '/load-cart',
-                        )
-                        products
-                            ? setShoppingCartProducts(products)
-                            : console.log(
-                                  "User has no products in cart or products couldn't be fetched",
-                              )
-                    }
+                    const loadCart = async () => await getCart()
                     loadCart()
                 }
             } else {
-                if (!username) {
-                    const checkIfUser = async () => {
-                        setIsLoading(true)
-                        setTries(0)
-                        const user = await loadResource('json', '/refresh-user')
-                        if (user.error) {
-                            logOut()
-                        }
-                        console.log('response.status', user?.status)
-                        if (user?.first_name) {
-                            console.log('RETURNED USER AFTER FIRST LOAD', user)
-                            setUsername(user.first_name)
-                            setLoggedIn(true)
-                        }
-                    }
+                if (!username && firstLoad) {
+                    const checkIfUser = async () => await getUser()
                     checkIfUser()
+
+                    console.log('CHECKING IF USER EXISTS')
                     if (!Array.isArray(items) || !items.length) {
-                        const loadItems = async () => {
-                            setTries(0)
-                            setIsLoading(true)
-                            const items = await loadResource('json')
-                            setItems(items.info)
-                        }
+                        const loadItems = async () => await getItems()
                         loadItems()
                     }
                 }
+                //TODO: remove this and add individual components to load static elements
                 const loadStatic = async () => {
                     await loadStaticElements()
-
                     console.log('LOADED PRIVACY POLICY')
                 }
                 loadStatic()
@@ -95,16 +121,29 @@ function useApi() {
         }
     }, [loggedIn])
 
+    //TODO: handle tries properly, maybe with an interval
     async function loadResource(
         responseType: 'json' | 'text',
         endpoint: string | null = null,
         headerOpts = { 'Content-type': 'application/json' },
     ) {
+        const retries = 3
         const url = endpoint ? `${API}${endpoint}` : API
-        setTimeout(() => setTries(tries + 1), 1000)
-        const resource = await getData(url, responseType, headerOpts)
-        if (tries === 3) return false
-        return resource
+        for (let i = 0; i < retries; i++) {
+            try {
+                const resource = await getData(url, responseType, headerOpts)
+                if (resource) {
+                    return resource
+                }
+            } catch (error) {
+                if (i < retries) {
+                    console.log('Failed to load resource, retrying...')
+                    await new Promise(resolve => setTimeout(resolve, 100))
+                } else {
+                    throw new Error('Failed to load resource')
+                }
+            }
+        }
     }
 
     async function loadStaticElements() {
