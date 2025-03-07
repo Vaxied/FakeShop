cb30bf3232166065fd0e05d799e31b2cab5e50b3
Vaxied <psgmarcelo@gmail.com>
2025-03-07 03:05:30 -0400

    (styles): added some styles to ProductSearchBar

--- frontend/src/components/product/ProductSearchBar/index.tsx
+++ frontend/src/components/product/ProductSearchBar/index.tsx
@@ -9,14 +9,23 @@ function ProductSearchBar(props: Readonly<searchProps>) {
     const { searchByTitle, setSearchByTitle } = props
     return (
         <div className='flex justify-center items-center mb-8'>
-            <div className='flex items-center bg-secondary/70 border border-accent rounded-lg px-4 py-2 outline-none w-80'>
+            <div className='flex items-center gap-2 bg-white border-2 border-accent has-[:focus]:border-secondary rounded-lg px-4 py-2 outline-none w-80 text-lg'>
+                <input
+                    type='text'
+                    placeholder='Search for a product'
+                    value={searchByTitle}
+                    onChange={event => {
+                        setSearchByTitle(event.target.value)
+                    }}
+                    className=' indent-4 outline-none bg-transparent placeholder-primary-text-color flex-1 text-secondary peer focus:border-secondary focus:placeholder-secondary border-none'
+                />
                 <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
                     viewBox='0 0 24 24'
                     strokeWidth={1.5}
                     stroke='currentColor'
-                    className='size-6 left-2'
+                    className='size-7 left-2 stroke-secondary peer-focus:stroke-accent'
                 >
                     <path
                         strokeLinecap='round'
@@ -24,15 +33,6 @@ function ProductSearchBar(props: Readonly<searchProps>) {
                         d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
                     />
                 </svg>
-                <input
-                    type='text'
-                    placeholder='Search a product'
-                    value={searchByTitle}
-                    onChange={(event) => {
-                        setSearchByTitle(event.target.value)
-                    }}
-                    className=' indent-4 outline-none bg-transparent placeholder-gray-700 flex-1'
-                />
             </div>
         </div>
     )
