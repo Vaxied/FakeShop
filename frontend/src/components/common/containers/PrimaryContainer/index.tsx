46a3ec6201cd9d26feef8eed2a57096d4efaef87
Vaxied <psgmarcelo@gmail.com>
2025-03-07 02:57:19 -0400

    (styles): added dynamic padding options to PrimaryContainer

--- frontend/src/components/common/containers/PrimaryContainer/index.tsx
+++ frontend/src/components/common/containers/PrimaryContainer/index.tsx
@@ -1,7 +1,21 @@
-type props = { children: React.ReactNode }
-function PrimaryContainer({ children }: Readonly<props>) {
+type props = {
+    paddingSize?: 'sm' | 'md' | 'lg' | 'xl' | 'none'
+    children: React.ReactNode
+}
+
+function PrimaryContainer({ children, paddingSize = 'lg' }: Readonly<props>) {
+    const containerPaddingSizes = {
+        none: 'md:px-0 md:py-0',
+        sm: 'md:px-4 md:py-3',
+        md: 'md:px-6 md:py-4',
+        lg: 'md:px-8 md:py-6',
+        xl: 'md:px-10 md:py-8',
+    }
+
     return (
-        <div className='w-full border border-gray p-3 md:px-6 md:py-4 rounded-lg bg-gray-100'>
+        <div
+            className={`w-full border border-gray p-3 rounded-lg bg-container ${containerPaddingSizes[paddingSize]}`}
+        >
             {children}
         </div>
     )
